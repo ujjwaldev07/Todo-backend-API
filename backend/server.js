dfaaa7da86c5125b7ba147e66b5383c4cb5bc3f1
sweetsoul5 <vilenofficial664@gmail.com>
2026-09-03 20:17:02 +0530

    updated server  and gitignore file

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
 const app = require('./app');
 const PORT = process.env.PORT || 6000;
 
+app.get('/', (req, res) => {
+    console.log('Todo Backend API is running');
+})
+
 app.listen(PORT, () => {
     console.log(`server is listening on ${PORT}`)
 })
